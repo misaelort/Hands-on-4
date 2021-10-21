f1753d1e663896ffa7cf2e94783068846ed9b2c0
Misael Ortega Gonzalez <84298819+misaelort@users.noreply.github.com>
2021-10-21 00:21:30 -0500

    Update HandsOn4.java

--- SimpleLinearRegression/HandsOn4.java
+++ SimpleLinearRegression/HandsOn4.java
@@ -1,4 +1,4 @@
-package examples.SLR;
+package examples.HandsOn4;
 
 import jade.core.Agent;
 import jade.core.AID;
@@ -9,7 +9,7 @@ public class HandsOn4 extends Agent {
 
 
 	private Interfaz myGui;  //Objeto de mi interfaz
-	double a,b;		  //Valores globales de b0 y b1
+	
 
 	
 	protected void setup() {
@@ -26,112 +26,205 @@ public class HandsOn4 extends Agent {
 	
 	public void action(){
 	
-	   int[] x = {23,26,30,34,43,48,52,57,58};
-           int[] y = {651,762,856,1063,1190,1298,1421,1440,1518};
-           double _a=1;
-           double _b=1;
-           double alpha=0.001;
-           int iters=100000;
-           
-   
-        System.out.println("Actividad de regresion lineal");
-
-
-        int sum_xy = 0, sum_x = 0, sum_y = 0, sum_x2 = 0;  
-
-        for(int actual = 0; actual < 9; actual++)
-        {
-            sum_xy += x[actual] * y[actual];
-            sum_x  += x[actual];
-            sum_y  += y[actual];
-            sum_x2 += Math.pow(x[actual], 2);
-        }
-        
-        System.out.println("SOLUCION ANALITICA");
-        System.out.println("Calculando valor de a (Punto de Corte)");
-
-         a = (double)(((sum_y * sum_x2) - (sum_x * sum_xy))/ ((9 * sum_x2) - Math.pow(sum_x, 2)));
-
-        System.out.println("Listo! El valor de a es: " + a);
-        
-        System.out.println("Calculando valor de b (Pendiente)");
-
-         b = (double)(((9 * sum_xy) - (sum_x * sum_y)) / ((9 * sum_x2) - Math.pow(sum_x,2)));   
-        
-        System.out.println("Listo! El valor de b es: " + b);
-        
-          	
-         //Imprimiendo Todos los Valores 
-	
-	for(int j=0;j<x.length;j++)
-	{
-		//System.out.println("Cuando x es " +x[j]+" y es " + (a+(b*x[j])));
-	}
-	
-	descenso_gradiente(x,y,_a,_b,alpha,iters);
-	
+	Regresion_Lineal op = new Regresion_Lineal();
+	op.imprimirRegresionAnalitica();
+	Regresion_Lineal_Gradiente op1 = new Regresion_Lineal_Gradiente();
+	op1.imprimirRegresionGradiente();   //imprimir
+	 
 	
 	 myGui.showGui();
-		}
-	}
+		}//Termina action
+	} // Termina clase RegresionLineal extends OneShotBehaviour
+	
 	
 	public void CalculaRegresion()
 	{
-	
-	System.out.println("Para el Valor " +myGui.numero+" Vale " + (a+(b*myGui.numero)));
+		Regresion_Lineal ob1 = new Regresion_Lineal();
+		ob1.CalculaRegresionAnalitica(myGui.numero);
+		Regresion_Lineal_Gradiente ob2 = new Regresion_Lineal_Gradiente();
+		ob2.CalculaRegresionGradiente(myGui.numero);
+		System.out.println("");
 	
 	}
 	
 	
-	public double coste(int _x[], int _y[], double _a, double _b)
+	public class Dataset
 	{
-	double m=9;
-	double error=0;
-	double hipotesiss=0;
-
-		for (int i=0; i<9; i++)
-		{
-			hipotesiss = _a+_b*_x[i];
-			error += (_y[i] - hipotesiss)*(_y[i] - hipotesiss);
-			error = error / (2*m);
-		}
+		private int[] x = {23,26,30,34,43,48,52,57,58};
+		private int[] y = {651,762,856,1063,1190,1298,1421,1440,1518};
 		
-	return error / (2*m);
-	}
-	
-	public void descenso_gradiente(int _x[], int _y[], double _a, double _b, double _alpha, int _epochs)
-	{
-	double m=9;
-	double error=0;
-	double hipotesis=0;
-	double b_deriv;
-	double a_deriv;
-
 		
-		for (int i=0; i<_epochs; i++)
-		{
-			b_deriv=0;
-			a_deriv=0;
-			
-			for (int j=0; j<9; j++)
+			public int[] GetX()
 			{
-				hipotesis = _a+_b*_x[j];
-				a_deriv += hipotesis - _y[j];
-				b_deriv += (hipotesis - _y[j]) * _x[j];
-				error=0;
-				error= coste(_x, _y, _a, _b);
-				if(error<0.0)
-				{break;}
+				return x;
 			}
-			_a -= (a_deriv / m) * _alpha;
-			_b -= (b_deriv / m) * _alpha;
-		}
-	System.out.println("SOLUCION CON GRADIENTE DESCENDENTE: ");
-	System.out.println("Listo! El valor de beta0 es: " + _a);
-	System.out.println("Listo! El valor de beta1 es: " + _b);		
-	System.out.println("Listo! El valor de error es: " + error);	
+			public int[] GetY()
+			{
+				return y;
+			}
+					
 	}
 	
 	
+	public class Regresion_Lineal extends Dataset 
+	{
+		private int sum_xy = 0, sum_x = 0, sum_y = 0, sum_x2 = 0;  
+		private double beta0, beta1;
+									
+			public void CalcularSumatorias()
+			{
+       			 for(int actual = 0; actual < 9; actual++)
+       		 	{
+           		 	sum_xy += GetX()[actual] * GetY()[actual];
+            		 	sum_x  += GetX()[actual];
+            		 	sum_y  += GetY()[actual];
+           		 	sum_x2 += Math.pow(GetX()[actual], 2);
+       		 	}
+			}		
+		
+			public void CalcularBeta1_Beta0()
+			{
+				System.out.println("SOLUCION ANALITICA:");
+				System.out.println("");
+        			System.out.println("Calculando valor de beta0 (Punto de Corte)");				
+				beta0 = (double)(((sum_y * sum_x2) - (sum_x * sum_xy))/ ((9 * sum_x2) - Math.pow(sum_x, 2)));
+
+				System.out.println("Listo! El valor de beta0 es: " + beta0);
+        
+				System.out.println("Calculando valor de beta1 (Pendiente)");
+
+				beta1 = (double)(((9 * sum_xy) - (sum_x * sum_y)) / ((9 * sum_x2) - Math.pow(sum_x,2)));   
+        			System.out.println("Listo! El valor de beta1 es: " + beta1);
+        			System.out.println("");
+			}
+			
+			public void Beta1_Beta0()
+			{
+				beta0 = (double)(((sum_y * sum_x2) - (sum_x * sum_xy))/ ((9 * sum_x2) - Math.pow(sum_x, 2)));
+				beta1 = (double)(((9 * sum_xy) - (sum_x * sum_y)) / ((9 * sum_x2) - Math.pow(sum_x,2)));   
+			}
+						
+			public void CalculaRegresionAnalitica(int _numero)
+			{
+				CalcularSumatorias();
+				Beta1_Beta0();
+				System.out.println("Calculado con Valores funcion Analitica");
+				System.out.println("Para el Valor " +_numero+" Vale " + (beta0+(beta1*_numero)));	
+			}		
+		
+			public void imprimirRegresionAnalitica()
+			{
+				CalcularSumatorias();
+				CalcularBeta1_Beta0();
+			}
+			
+	}  // End of inner class Regresion_Lineal
+	
+
+
+
+
+
+
+	private class Regresion_Lineal_Gradiente extends Regresion_Lineal
+	{
+		private double m=9, error=0,hipotesis=0,iters=1000000;
+		private double a_deriv,b_deriv;
+		private double _a=1,_b=1,_alpha=0.001;
+		private double iter=1e-2;
+		
+			public double coste(int _x[], int _y[], double _a, double _b)
+			{
+		
+				double errorcoste=0;
+				double hipotesiss=0;
+
+
+				for (int i=0; i<9; i++)
+				{
+					hipotesiss = _a+_b*_x[i];
+					errorcoste += (_y[i] - hipotesiss)*(_y[i] - hipotesiss);
+					errorcoste = errorcoste / (2*m);
+				}
+		
+				return errorcoste / (2*m);
+			}
+			
+			public void beta0_beta1()
+			{
+				for (int i=0; i<iters; i++)
+				{
+					b_deriv=0;
+					a_deriv=0;
+			
+					for (int j=0; j<9; j++)
+					{
+						hipotesis = _a+_b*GetX()[j];
+						a_deriv += hipotesis - GetY()[j];
+						b_deriv += (hipotesis - GetY()[j]) * GetX()[j];
+						error=0;
+						error= coste(GetX(), GetY(), _a, _b);   //GetX()
+						
+						if(error<iter)
+						{
+							break;
+						}
+					}
+					
+					_a -= (a_deriv / m) * _alpha;
+					_b -= (b_deriv / m) * _alpha;
+				
+				}			
+			}
+	
+			public void CalculaDescensoGradiente()
+			{
+				for (int i=0; i<iters; i++)
+				{
+					b_deriv=0;
+					a_deriv=0;
+			
+					for (int j=0; j<9; j++)
+					{
+						hipotesis = _a+_b*GetX()[j];
+						a_deriv += hipotesis - GetY()[j];
+						b_deriv += (hipotesis - GetY()[j]) * GetX()[j];
+						error=0;
+						error= coste(GetX(), GetY(), _a, _b);   //GetX()
+						
+						if(error<iter)
+						{
+							break;
+						}
+					}
+					
+					_a -= (a_deriv / m) * _alpha;
+					_b -= (b_deriv / m) * _alpha;
+				
+				}
+	
+			System.out.println("SOLUCION CON GRADIENTE DESCENDENTE:");
+			System.out.println("");
+			System.out.println("Listo! El valor de beta0 es: " + _a);
+			System.out.println("Listo! El valor de beta1 es: " + _b);		
+			System.out.println("Listo! El valor de error es: " + error);	
+			System.out.println("");	
+	
+			}
+			
+			public void CalculaRegresionGradiente(int _numero)
+			{
+				beta0_beta1();
+				System.out.println("Calculado con Valores funcion Gradiente");
+				System.out.println("Para el Valor " +_numero+" Vale " + (_a+(_b*_numero)));	
+			}
+	
+			public void imprimirRegresionGradiente()
+			{
+				
+				CalculaDescensoGradiente();
+			}
+	
+	}  // End of inner class Regresion_Lineal_Gradiente
 
 }
